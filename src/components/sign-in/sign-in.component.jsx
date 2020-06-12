01e23781b7cd09a8ed3114cf71b118724a3bdc03
Manuel Perez Torres <manuel081089@gmail.com>
2020-06-12 17:47:31 -0400

    Fix bug with input form :bug:

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {signInWithGoogle, auth} from '../../firebase/firebase.utils'
 import './sign-in.styles.scss'
 
 export default class SignIn extends Component {
@@ -13,11 +13,16 @@ export default class SignIn extends Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({
-      email: '', password: ''
-    })
+
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email:'', password: ''})
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   handleChange = event => {
